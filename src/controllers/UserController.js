92169e3a02c64502ab2f551af0d4a435f420d37d
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2021-10-26 20:13:29 -0300

    Criando o User Controler

---
+++ src/controllers/UserController.js
@@ -0,0 +1,70 @@
+const UserModel = require("../models/UserModel");
+
+module.exports = {
+  async create(request, response){
+    try {
+      const newUser = request.body;
+
+      const result = await UserModel.create(newUser);
+
+      return response.status(200).json({user_id: result});
+
+    } catch (error) {
+      console.warn("User creation failed", error);
+
+      return response.status(500).json({
+        notification: "Internal server error while trying to create User",
+      })
+    }
+  },
+
+  async  getById(request, response){
+    try {
+      const { user_id } = request.params;
+      const result = await UserModel.getById(user_id);
+
+      return response.status(200).json(result);
+
+    } catch (error) {
+      console.log("User getById failed" + error);
+
+      return response.status(500).json({
+        notification: "Internal server error while trying to get User",
+      })
+    }
+  },
+
+  async update(request, response){
+    try {
+      const { user_id } = request.params;
+      const user = request.body;
+      const result = await UserModel.updateById(user_id, user);
+
+      return response.status(200).json(result);
+    } catch (error) {
+      console.log("User update failed" + error);
+
+      return response.status(500).json({
+        notification: "Internal server error while trying to update User",
+      })
+    }
+  },
+
+  async delete(request, response){
+    try {
+      const { user_id } = request.params;
+      const result = await UserModel.deleteById(user_id);
+
+      return response.status(200).json(result);
+  
+    } catch (error) {
+      console.log("User delete failed" + error);
+
+      return response.status(500).json({
+        notification: "Internal server error while trying to delete User",
+      })
+    }
+  },
+
+  
+}
