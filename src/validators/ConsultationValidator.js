5464f5239d947039091b00e65206c74ea6215ba7
Adrianus Alexsander <adrianusvieira@cpejr.com.br>
2021-11-02 14:31:22 -0300

    Consertando Migrations

--- src/validators/ConsultationValidator.js
+++ src/validators/ConsultationValidator.js
@@ -4,21 +4,26 @@ module.exports = {
   create: celebrate({
     [Segments.BODY]: Joi.object().keys({
       date: Joi.date().required(),
+      hour: Joi.string().required(),
     }),
-    [Segments.HEADERS]: Joi.object().keys({
-      authorization: Joi.string().required(),
-    })
-    .unknown(),
+    [Segments.HEADERS]: Joi.object()
+      .keys({
+        authorization: Joi.string().required(),
+      })
+      .unknown(),
   }),
 
   update: celebrate({
     [Segments.BODY]: Joi.object().keys({
       date: Joi.date().optional(),
-    })
+      hour: Joi.string().optional(),
+    }),
   }),
   delete: celebrate({
     [Segments.PARAMS]: Joi.object().keys({
-      consultattion_id: Joi.string().guid({ version: ["uuidv4"]}).required(),
+      consultattion_id: Joi.string()
+        .guid({ version: ["uuidv4"] })
+        .required(),
     }),
-  })
-}
+  }),
+};
